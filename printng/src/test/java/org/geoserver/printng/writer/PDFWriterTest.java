0ab2e051da931d60ef564b1c72a00d54a014bbd4
Robert Marianski <rob@marianski.com>
2012-07-12 00:28:55 -0400

    add pdf writer test

---
+++ printng/src/test/java/org/geoserver/printng/writer/PDFWriterTest.java
@@ -0,0 +1,29 @@
+package org.geoserver.printng.writer;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.geoserver.printng.resource.PrintngRestDocumentParser;
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+public class PDFWriterTest {
+
+    @Test
+    public void testWrite() throws IOException {
+        String input = "<div>foobar</div>";
+        PrintngRestDocumentParser parser = new PrintngRestDocumentParser(new StringReader(input));
+        Document document = parser.parse();
+        PDFWriter pdfWriter = new PDFWriter(document);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        pdfWriter.write(byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        byte[] magicBytes = new byte[4];
+        System.arraycopy(bytes, 0, magicBytes, 0, 4);
+        String magic = new String(magicBytes);
+        assertEquals("invalid pdf bytes", "%PDF", magic);
+    }
+}
