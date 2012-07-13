5ee17842ce6d62ab6b5349558ef28a64d6089fe5
Robert Marianski <rob@marianski.com>
2012-07-13 01:11:19 -0400

    simpler xml serialization in test

--- printng/src/test/java/org/geoserver/printng/resource/PrintngRestDocumentParserTest.java
+++ printng/src/test/java/org/geoserver/printng/resource/PrintngRestDocumentParserTest.java
@@ -6,29 +6,24 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
+import org.apache.xml.serialize.XMLSerializer;
 import org.junit.Test;
 import org.w3c.dom.Document;
 
 public class PrintngRestDocumentParserTest {
 
     @Test
-    public void testParse() throws IOException, TransformerException {
+    public void testParse() throws IOException {
         String input = "<div>foobar</div>";
         StringReader stringReader = new StringReader(input);
         PrintngRestDocumentParser parser = new PrintngRestDocumentParser(stringReader);
         Document document = parser.parse();
-        TransformerFactory txFactory = TransformerFactory.newInstance();
-        Transformer tx = txFactory.newTransformer();
+
         StringWriter stringWriter = new StringWriter();
-        tx.transform(new DOMSource(document), new StreamResult(stringWriter));
+        XMLSerializer serializer = new XMLSerializer(stringWriter, null);
+        serializer.serialize(document);
         String result = stringWriter.getBuffer().toString();
-        String exp = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><div>foobar</div>";
+        String exp = "<?xml version=\"1.0\"?>\n<div>foobar</div>";
         assertEquals("Invalid document parse", exp, result);
     }
 
