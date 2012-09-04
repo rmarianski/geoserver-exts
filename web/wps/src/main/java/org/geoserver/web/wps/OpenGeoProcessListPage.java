d7c12c23838e041b4b1b582fd34d232bc93c767d
David Winslow <cdwinslow@gmail.com>
2012-09-04 15:28:10 -0400

    Corrections and tweaks in WPS Process Index page

--- web/wps/src/main/java/org/geoserver/web/wps/OpenGeoProcessListPage.java
+++ web/wps/src/main/java/org/geoserver/web/wps/OpenGeoProcessListPage.java
@@ -8,6 +8,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -27,6 +28,9 @@ import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
+import org.geoserver.ows.URLMangler;
+import org.geoserver.ows.URLMangler.URLType;
+import org.geoserver.ows.util.ResponseUtils;
 import org.geoserver.web.GeoServerBasePage;
 import org.geoserver.web.wicket.GeoServerDataProvider.Property;
 import org.geoserver.web.wicket.GeoServerTablePanel;
@@ -77,10 +81,17 @@ public class OpenGeoProcessListPage extends GeoServerBasePage {
                 
                 ProcessDescriptor desc = pd.getObject();
                 ProcessFactory factory = Processors.createProcessFactory(desc.getQualifiedName());
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("service", "wps");
+                params.put("version", "1.0.0");
+                params.put("request", "DescribeProcess");
+                params.put("identifier", desc.getName());
+                String wpsDescribeProcessRequest = ResponseUtils.buildURL("../", "wps", params, URLMangler.URLType.SERVICE);
                 details.add(
                         new Label("title", factory.getTitle(desc.getQualifiedName()).toString()),
                         new Label("description", new PropertyModel<String>(pd, "description")),
                         new Label("name", desc.getName()),
+                        new ExternalLink("describeprocess", wpsDescribeProcessRequest),
                         createParameterList("input", factory.getParameterInfo(desc.getQualifiedName()).values()),
                         createParameterList("output", factory.getResultInfo(desc.getQualifiedName(), Collections.<String, Object>emptyMap()).values()));
                 
@@ -98,37 +109,65 @@ public class OpenGeoProcessListPage extends GeoServerBasePage {
 	}
 	
 	@SuppressWarnings("serial")
-    private static ListView<ParameterInfo> createParameterList(final String id, final Collection<Parameter<?>> params) {
+    private Component createParameterList(final String id, final Collection<Parameter<?>> params) {
 	    List<ParameterInfo> inputs = new ArrayList<ParameterInfo>();
 	    
 	    for (Parameter<?> input : params) {
-	        inputs.add(new ParameterInfo(input.getTitle().toString(), input.getType().getSimpleName()));
+	        inputs.add(new ParameterInfo(
+	                input.getName().toString(),
+	                input.getDescription().toString(),
+	                input.getType().getSimpleName(),
+	                input.getMinOccurs(),
+	                input.getMaxOccurs()));
+	        
 	    }
 	    
-        return new ListView<ParameterInfo>(id, inputs) {
+	    Fragment table = new Fragment(id, "paramlist", OpenGeoProcessListPage.this);
+	    ListView<ParameterInfo> rows = new ListView<ParameterInfo>("param", inputs) {	    
             @Override
             protected void populateItem(ListItem<ParameterInfo> item) {
                 item.add(
-                        new Label("name", new PropertyModel<String>(item.getModel(), "title")),
-                        new Label("type", new PropertyModel<Class<?>>(item.getModel(), "type")));
+                    new Label("title", new PropertyModel<String>(item.getModel(), "title")),
+                    new Label("name", new PropertyModel<String>(item.getModel(), "name")),
+                    new Label("type", new PropertyModel<String>(item.getModel(), "type")),
+                    new Label("minOccurs", new PropertyModel<Integer>(item.getModel(), "minOccurs")),
+                    new Label("maxOccurs", new PropertyModel<String>(item.getModel(), "maxOccurs")).setEscapeModelStrings(false));
             }
         };
+        
+        table.add(rows);
+        return table;
 	}
 	
 	@SuppressWarnings("serial")
     private static class ParameterInfo implements Serializable {
+	    private final String name;
 	    private final String title;
 	    private final String type;
+	    private final int minOccurs;
+	    private final String maxOccurs;
 	    
-	    public ParameterInfo(String title, String type) {
+	    public ParameterInfo(String name, String title, String type, int minOccurs, int maxOccurs) {
+	        this.name = name;
 	        this.title = title;
 	        this.type = type;
+	        this.minOccurs = minOccurs;
+	        this.maxOccurs = (maxOccurs < Integer.MAX_VALUE) ? String.valueOf(maxOccurs) : "&infin;";
 	    }
+	    
+	    @SuppressWarnings("unused")
+	    public String getName() { return name; }
 	    
 	    @SuppressWarnings("unused")
         public String getTitle() { return title; }
 	    
 	    @SuppressWarnings("unused")
         public String getType() { return type; }
+	    
+	    @SuppressWarnings("unused")
+	    public String getMaxOccurs() { return maxOccurs; }
+	    
+        @SuppressWarnings("unused")
+        public int getMinOccurs() { return minOccurs; }
 	}
 }
