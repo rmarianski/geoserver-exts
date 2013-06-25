9a3d179bcb0310f7d99bfaa696711fb00a5863b1
Robert Marianski <rob@marianski.com>
2013-06-25 16:43:29 -0400

    minor logging changes

--- monitor-console/src/main/java/org/opengeo/console/monitor/transport/HttpMessageTransport.java
+++ monitor-console/src/main/java/org/opengeo/console/monitor/transport/HttpMessageTransport.java
@@ -1,8 +1,6 @@
 package org.opengeo.console.monitor.transport;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.Collection;
 import java.util.logging.Level;
@@ -41,18 +39,17 @@ public class HttpMessageTransport implements ConsoleMessageTransport {
         PostMethod postMethod = new PostMethod(url);
 
         JSONObject json = consoleMessageSerializer.serialize(data);
+        String jsonPayload = json.toString();
 
         StringRequestEntity requestEntity = null;
         try {
-            requestEntity = new StringRequestEntity(json.toString(), "application/json", "UTF-8");
+            requestEntity = new StringRequestEntity(jsonPayload, "application/json", "UTF-8");
         } catch (UnsupportedEncodingException e) {
             Throwables.propagate(e);
         }
         postMethod.setRequestEntity(requestEntity);
 
-        if (LOGGER.isLoggable(Level.FINE)) {
-            LOGGER.fine(json.toString());
-        }
+        LOGGER.fine(jsonPayload);
 
         // send message
         try {
@@ -76,10 +73,7 @@ public class HttpMessageTransport implements ConsoleMessageTransport {
     private void logCommunicationError(Exception e) {
         LOGGER.warning("Error communicating with: " + url);
         if (LOGGER.isLoggable(Level.INFO)) {
-            StringWriter out = new StringWriter();
-            PrintWriter writer = new PrintWriter(out);
-            e.printStackTrace(writer);
-            LOGGER.info(out.toString());
+            LOGGER.info(Throwables.getStackTraceAsString(e));
         }
     }
 
