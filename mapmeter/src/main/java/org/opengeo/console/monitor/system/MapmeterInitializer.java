8477bc05d99951d625b4a2cc27d20003f2a9dcd7
Robert Marianski <rob@marianski.com>
2013-08-29 17:50:46 -0400

    send system data in separate thread

--- mapmeter/src/main/java/org/opengeo/console/monitor/system/MapmeterInitializer.java
+++ mapmeter/src/main/java/org/opengeo/console/monitor/system/MapmeterInitializer.java
@@ -25,27 +25,53 @@ public class MapmeterInitializer implements GeoServerInitializer {
 
     @Override
     public void initialize(GeoServer geoServer) throws Exception {
-        Optional<SystemData> maybeSystemData = supplier.get();
-        if (!maybeSystemData.isPresent()) {
-            LOGGER.warning("Mapmeter api key not configured. Not sending system data.");
-            return;
+        // run in separate thread so we don't block startup
+        // not sure if getting the manifest data is expensive, but it's probably io
+        // sending the data could definitely block depending on mapmeter server response
+        // hopefully pulling the manifest data this way is thread safe
+        TransportTask transportTask = new TransportTask(supplier, transport);
+        transportTask.start();
+    }
+
+    private static class TransportTask extends Thread implements Runnable {
+
+        private final SystemDataTransport transport;
+
+        private final SystemDataSupplier supplier;
+
+        public TransportTask(SystemDataSupplier supplier, SystemDataTransport transport) {
+            this.supplier = supplier;
+            this.transport = transport;
         }
-        SystemData systemData = maybeSystemData.get();
-        SystemDataTransportResult result = transport.transport(systemData);
-        if (result.isSuccessful()) {
-            LOGGER.info("System data successfully sent to Mapmeter");
-        } else {
-            if (result.isTransferError()) {
-                Exception error = result.getError().get();
-                LOGGER.severe("Error transferring data to Mapmeter: " + error.getLocalizedMessage());
-                if (LOGGER.isLoggable(Level.INFO)) {
-                    LOGGER.info(Throwables.getStackTraceAsString(error));
-                }
+
+        @Override
+        public void run() {
+            Optional<SystemData> maybeSystemData = supplier.get();
+            if (!maybeSystemData.isPresent()) {
+                LOGGER.warning("Mapmeter api key not configured. Not sending system data.");
+                return;
+            }
+
+            SystemData systemData = maybeSystemData.get();
+            SystemDataTransportResult result = transport.transport(systemData);
+            if (result.isSuccessful()) {
+                LOGGER.info("System data successfully sent to Mapmeter");
             } else {
-                LOGGER.severe("Error response from mapmeter. Code: " + result.getStatusCode().get()
-                        + " - Response text: " + result.getResponseText().get());
+                if (result.isTransferError()) {
+                    Exception error = result.getError().get();
+                    LOGGER.severe("Error transferring data to Mapmeter: "
+                            + error.getLocalizedMessage());
+                    if (LOGGER.isLoggable(Level.INFO)) {
+                        LOGGER.info(Throwables.getStackTraceAsString(error));
+                    }
+                } else {
+                    LOGGER.severe("Error response from mapmeter. Code: "
+                            + result.getStatusCode().get() + " - Response text: "
+                            + result.getResponseText().get());
+                }
             }
         }
+
     }
 
 }
